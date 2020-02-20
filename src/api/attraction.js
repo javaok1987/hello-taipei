11b44d55aa9069cd49068c4408f624a7ddc130e6
Ian Liao <javaok1987@gmail.com>
2020-02-20 18:29:47 +0800

    :tada: 發布測試頁面

--- src/api/attraction.js
+++ src/api/attraction.js
@@ -4,5 +4,6 @@ import request from '@/common/request';
  * 查詢景點.
  */
 export function fetchAttractionList(data) {
-  return request.get('/api/open-api/zh-tw/Attractions/All', data);
+  // return request.get('/api/open-api/zh-tw/Attractions/All', data);
+  return request.get('//raw.githubusercontent.com/javaok1987/hello-taipei/master/src/data/Attractions.json', data);
 }
